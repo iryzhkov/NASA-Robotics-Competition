c9673c8903cc656d5c2d858475e2befd1f5c34f2
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2016-02-01 22:13:55 -0700

    Created Sensor_Logic header

---
+++ Sensor_Logic.h
@@ -0,0 +1,8 @@
+/*  CODE FOR NASA ROBOTICS CHALLANGE 2016
+ *  by Igor Ryzhkov (igor.o.ryzhkov@gmail.com)
+ *  
+ *  This class assumes the configuration of sensors, that we have for the robot. 
+ */
+
+class Sensor_Logic {
+};
