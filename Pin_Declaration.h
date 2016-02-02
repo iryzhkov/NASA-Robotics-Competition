d0f71272518da3f3499f40f5ddd445d54df42423
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2016-02-01 22:11:38 -0700

    Created pin declaration header

---
+++ Pin_Declaration.h
@@ -0,0 +1,18 @@
+/*  CODE FOR NASA ROBOTICS CHALLANGE 2016
+ *  by Igor Ryzhkov (igor.o.ryzhkov@gmail.com)
+ *  
+ *  This header defines all the pins, that are going to be used in the project
+ */ 
+
+// declaration of motor pins
+#define right_motor_dir_pin 11
+#define right_motor_pwm_pin 6
+#define left_motor_dir_pin 12
+#define left_motor_pwm_pin 5
+
+// declaration of sensor pins
+#define left_hole_sensor_pin A0
+#define right_hole_sensor_pin A1
+#define left_obstacle_sensor_pin A2
+#define right_obstacle_sensor_pin A3
+#define middle_obstacle_sensor_pin A4
