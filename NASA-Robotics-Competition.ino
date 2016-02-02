1a4719b2081fb79ce9775515a65f8ce9c6500f67
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2016-02-01 22:13:13 -0700

    Added global variables

--- NASA-Robotics-Competition.ino
+++ NASA-Robotics-Competition.ino
@@ -26,6 +26,42 @@ void setup()
   pinMode(LED, OUTPUT);
 
   timer_setup(); // setting continious data acquisition.
+Beacon_Sensor     *beacon_Sensor;
+
+IR_Sensor         *left_Hole_Sensor,
+                  *right_Hole_Sensor,
+                  *left_Obstacle_Sensor,
+                  *right_Obstacle_Sensor,
+                  *middle_Obstacle_Sensor;
+
+void setup() {
+    Serial.begin(9600); // starting serial communication.
+
+    // setting up the motors
+    left_Motor = new Motor (left_motor_pwm_pin, left_motor_dir_pin);
+    right_Motor = new Motor (right_motor_pwm_pin, right_motor_dir_pin);
+
+    // setting up Movement_Control
+    drive_Control = new Movement_Control(left_Motor, right_Motor);
+
+    // setting up IR sensors
+    // left_Hole_Sensor = new IR_Sensor(left_hole_sensor_pin);
+    // right_Hole_Sensor = new IR_Sensor(right_hole_sensor_pin);
+    // left_Obstacle_Sensor = new IR_Sensor(left_obstacle_sensor_pin);
+    // right_Obstacle_Sensor = new IR_Sensor(right_obstacle_sensor_pin);
+    // middle_Obstacle_Sensor = new IR_Sensor(middle_obstacle_sensor_pin);
+
+    // setting up compass and beacon sensors
+    // compass_Sensor = new Compass_Sensor();
+    // beacon_Sensor = new Beacon_Sensor();
+
+    // setting up sensor logic
+
+    // setting up robot logic
+    // robot = new Robot_Logic (drive_Control, sensor_Logic);
+   
+  
+    timer_setup(); // setting continious data acquisition.
 }
 
 void loop() 
