1034ddf36b75107fc71101802278ca976ac88247
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2016-02-19 10:54:43 -0700

    minor adjustments

--- Unit_Tests.ino
+++ Unit_Tests.ino
@@ -9,46 +9,48 @@
  *  library link: https://github.com/mmurdoch/arduinounit
  */
 
-void Set_Sensors_To_Zero () {
+void Set_To_Zero_State () {
     left_pit_sensor->Set_Value(0);
     right_pit_sensor->Set_Value(0);
     left_sensor->Set_Value(0);
     right_sensor->Set_Value(0);
     middle_sensor->Set_Value(0);
     beacon_direction->Set_Value(0);
+
+    robot->Set_Process_ID (0);
 }
 
 test (correct_danger_id_for_no_dangers){
-    Set_Sensors_To_Zero();
+    Set_To_Zero_State();
     fail();
 }
 
 test (correct_danger_id_for_far_obstacle){
-    Set_Sensors_To_Zero();
+    Set_To_Zero_State();
     fail();
 }
 
 test (correct_side_id_for_far_obstacle) {
-    Set_Sensors_To_Zero();
+    Set_To_Zero_State();
     fail();
 }
 
 test (correct_danger_id_for_close_obstacles){
-    Set_Sensors_To_Zero();
+    Set_To_Zero_State();
     fail();
 }
 
 test (correct_side_id_for_close_obstacles){
-    Set_Sensors_To_Zero();
+    Set_To_Zero_State();
     fail();
 }
 
 test (correct_danger_id_for_pit){
-    Set_Sensors_To_Zero();
+    Set_To_Zero_State();
     fail();
 }
 
 test (correct_side_id_for_pit){
-    Set_Sensors_To_Zero();
+    Set_To_Zero_State();
     fail();
 }
